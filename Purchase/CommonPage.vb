f39d45a3f0e772451be63b5e0e7f7044dc84ad41
koyama <koyama@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-05-29 01:02:15 +0000

    OnLoad() 追加。
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@18 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/CommonPage.vb
+++ Purchase/CommonPage.vb
@@ -1,3 +1,14 @@
-﻿Public Class CommonPage
+﻿Imports System
+Imports System.Collections
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
 
+Public Class CommonPage
+    Inherits Page
+
+    Protected Overrides Sub OnLoad(ByVal e As System.EventArgs)
+        MyBase.OnLoad(e)
+
+        'User Authorization
+    End Sub
 End Class
