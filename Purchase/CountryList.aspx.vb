b755590a952b83d9bae96c88c9da4b014baa35e3
toshiteru <toshiteru@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-06-02 08:25:50 +0000

    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@32 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/CountryList.aspx.vb
+++ Purchase/CountryList.aspx.vb
@@ -2,7 +2,7 @@
     Inherits CommonPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        CountryList.DataBind()
     End Sub
 
 End Class
