7d258c9f50b2a9c3aaddf0ca697ac57f0a6d8f82
toshiteru <toshiteru@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-06-19 08:23:11 +0000

    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@119 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/SupplierSearch.aspx.vb
+++ Purchase/SupplierSearch.aspx.vb
@@ -4,41 +4,14 @@
 #Region " Web フォーム デザイナで生成されたコード "
     '*****（Region内は変更しないこと）*****
     <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
-        Me.SqlConnection1 = New System.Data.SqlClient.SqlConnection
-        Me.SqlConnection1.FireInfoMessageEventOnUserErrors = False
     End Sub
-
-    Protected WithEvents SqlConnection1 As System.Data.SqlClient.SqlConnection
 
     Private Sub Page_Init(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Init
         InitializeComponent()
     End Sub
 #End Region
 
-    '*****（DB接続用変数定義）*****
-    Dim DBConnString As String                              '接続文字列	
-    Dim DBConn As New System.Data.SqlClient.SqlConnection   'データベースコネクション	
-    Dim DBCommand As System.Data.SqlClient.SqlCommand       'データベースコマンド	
-    Dim DBReader As System.Data.SqlClient.SqlDataReader     'データリーダー	
-
-    Sub Set_DBConnectingString()
-        Dim settings As ConnectionStringSettings
-        '[接続文字列を設定ファイル(Web.config)から取得]---------------------------------------------
-        settings = ConfigurationManager.ConnectionStrings("DatabaseConnect")
-        If Not settings Is Nothing Then
-            '[接続文字列をイミディエイトに出力]-----------------------------------------------------
-            Debug.Print(settings.ConnectionString)
-        End If
-        '[sqlConnectionに接続文字列を設定]----------------------------------------------------------
-        Me.SqlConnection1.ConnectionString = settings.ConnectionString
-    End Sub
-
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        '[DBの接続]-----------------------------------------------------------------------
-        Set_DBConnectingString()
-        DBConn = Me.SqlConnection1
-        DBConn.Open()
-
         SrcSupplier.SelectCommand = ""
     End Sub
 
@@ -52,7 +25,7 @@
             Msg.Text = "SupplierCodeには数字を入力して下さい"
             SrcSupplier.SelectCommand = ""
             SupplierList.DataBind()
-        ElseIf not IsNumeric(R3Code.Text.ToString) then
+        ElseIf R3Code.Text.ToString <> "" And Not IsNumeric(R3Code.Text.ToString) Then
             Msg.Text = "R/3SupplierCodeには数字を入力して下さい"
             SrcSupplier.SelectCommand = ""
             SupplierList.DataBind()
