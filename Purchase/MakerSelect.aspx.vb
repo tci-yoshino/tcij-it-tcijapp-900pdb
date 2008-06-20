d27dfae42a72a5e7ceab7d70410dca17a0395510
akutsu <akutsu@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-06-20 04:19:12 +0000

    SQLクエリ修正。
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@126 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/MakerSelect.aspx.vb
+++ Purchase/MakerSelect.aspx.vb
@@ -15,14 +15,11 @@
         st_Location = Trim(st_Location)  ' 空白除去
 
         If String.IsNullOrEmpty(st_Location) Then
-
             ' パラメータ Location が無い場合はエラーメッセージを表示して終了。
             st_Errorr_Meggage = st_Errorr_Meggage & "見積依頼拠点コードが設定されていません。"
             ErrorMessages.Text = st_Errorr_Meggage
             Exit Sub
-
         Else
-
 
             ' パラメータ Code, Name を取得
             st_Code = IIf(Not (IsPostBack) And Not (String.IsNullOrEmpty(Request.QueryString("Code"))), Request.QueryString("Code"), Request.Form("Code"))
@@ -46,9 +43,9 @@
 
             ' ポストバックではない 且つ GET が 空, notiong, NULL 以外なら実行
             If Not (IsPostBack) And Not (String.IsNullOrEmpty(Request.QueryString("Code"))) Then
-                Dim dataSet As DataSet = New DataSet
+                Dim dataSet As DataSet = New DataSet("Supplier")
                 dataSet = Get_Supplier_Data(dataSet, DBConnectString.ConnectionString)
-                SupplierList.DataSource = dataSet
+                SupplierList.DataSource = dataSet.Tables("SupplierList")
                 SupplierList.DataBind()
             End If
 
@@ -57,131 +54,98 @@
 
     ' Search ボタンクリック処理
     Protected Sub Search_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Search.Click
+        ' st_Code と st_Name があれば実行
         If Not String.IsNullOrEmpty(st_Code) Or Not String.IsNullOrEmpty(st_Name) Then
-            Dim dataSet As DataSet = New DataSet
+            Dim dataSet As DataSet = New DataSet("Supplier")
             dataSet = Get_Supplier_Data(dataSet, DBConnectString.ConnectionString)
-            SupplierList.DataSource = dataSet
+            SupplierList.DataSource = dataSet.Tables("SupplierList")
             SupplierList.DataBind()
         End If
     End Sub
 
-    ' Supplier リスト取得処理
+    ' 仕入先リスト取得関数
     ' Public 変数の st_Code と st_Name を元に Supplier テーブルからデータを取得する。
+    '
     ' [パラメータ]
-    ' dataSet As DataSet: 取得したデータをセットする DataSet オブジェクト
-    ' connectionString As String) As String: 接続情報
+    ' dataSet 取得したデータをセットする DataSet オブジェクト
+    ' connectionString: 接続情報。文字列。
+    '
     ' [戻り値]
-    ' dataSet As DataSet : 取得したデータをセットした DataSet オブジェクト
+    ' dataSet: 取得したデータをセットした DataSet オブジェクト
+
     Public Function Get_Supplier_Data(ByVal dataSet As DataSet, ByVal connectionString As String) As DataSet
-        Dim st_where As String = " WHERE(SupplierCode = @Code) AND ({ fn CONCAT(Name1, Name2) } LIKE N'%' + @Name + '%') "
+
+        ' WHERE 分の分岐
+        Dim st_where As String = " WHERE(Supplier.SupplierCode = @Code) AND ({ fn CONCAT(Supplier.Name1, Supplier.Name2) } LIKE N'%' + @Name + '%') "
         If Not String.IsNullOrEmpty(st_Code) And String.IsNullOrEmpty(st_Name) Then
-            st_where = " WHERE(SupplierCode = @Code) "
+            st_where = " WHERE(Supplier.SupplierCode = @Code) "
         ElseIf String.IsNullOrEmpty(st_Code) And Not String.IsNullOrEmpty(st_Name) Then
-            st_where = "  WHERE({ fn CONCAT(Name1, Name2) } LIKE N'%' + @Name + '%')  "
+            st_where = "  WHERE({ fn CONCAT(Supplier.Name1, Supplier.Name2) } LIKE N'%' + @Name + '%')  "
         End If
 
-
-        Dim st_query As String = "SELECT SupplierCode, Name3, Name4, CountryCode FROM Supplier " & st_where & " ORDER BY SupplierCode, Name3 "
+        ' 仕入先リスト取得
+        ' [備考]
+        ' LEFT OUTER JOIN で連結した際、IrregularRFQLocation.QuoLocationCode の値が
+        ' レコードが存在していて NULL なのか、存在していなくて NULL なのかの判断ができないため、
+        ' IrregularRFQLocation.SupplierCode を取得し、この値が NULL の場合はレコードが取得「できなかった」と判断する。
+        Dim st_query As String = _
+  "SELECT " _
+& "  Supplier.SupplierCode, Supplier.Name3, Supplier.Name4, Supplier.CountryCode, " _
+& "  IrregularRFQLocation.SupplierCode AS IrregularSupplierCode, " _
+& " PurchasingCountry.DefaultQuoLocationCode, " _
+& "  IrregularRFQLocation.QuoLocationCode AS IrregularQuoLocationCode " _
+& "FROM  " _
+& "  Supplier " _
+& "    JOIN PurchasingCountry " _
+& "      ON Supplier.CountryCode = PurchasingCountry.CountryCode " _
+& "    LEFT OUTER JOIN IrregularRFQLocation  " _
+& "      ON Supplier.SupplierCode = IrregularRFQLocation.SupplierCode  " _
+& "         AND IrregularRFQLocation.EnqLocationCode = @Location " _
+& st_where _
+& "ORDER BY  " _
+& "  Supplier.SupplierCode, Supplier.Name3 "
 
         Try
             Using connection As New SqlClient.SqlConnection(connectionString)
 
+                ' 接続情報、アダプタ、SQLコマンド オブジェクトの生成
                 Dim adapter As New SqlClient.SqlDataAdapter()
                 Dim command As New SqlClient.SqlCommand(st_query, connection)
 
+                ' DataSet にテーブルとカラムを追加
+                dataSet.Tables.Add("SupplierList")
+                dataSet.Tables("SupplierList").Columns.Add("QuoLocationCode", Type.GetType("System.String"))
+
+                ' SQL SELECT パラメータの追加
                 command.Parameters.AddWithValue("Code", st_Code)
                 command.Parameters.AddWithValue("Name", st_Name)
                 command.Parameters.AddWithValue("Location", st_Location)
 
+                ' データベースからデータを取得
                 adapter.SelectCommand = command
-                adapter.Fill(dataSet)
+                adapter.Fill(dataSet.Tables("SupplierList"))
 
-                Dim dataTable As DataTable = New DataTable
-                dataTable = dataSet.Tables(0)
-                dataTable.Columns.Add("QuoLocationCode", Type.GetType("System.String"))
-
-                Dim cnt As Integer = dataTable.Rows.Count - 1
+                ' 見積回答拠点コード取得
                 Dim i As Integer
-                For i = 0 To cnt
-                    Dim QuoLocationCode = Get_Quo_Location_Code(dataTable.Rows(i).Item("SupplierCode"), dataTable.Rows(i).Item("CountryCode"), DBConnectString.ConnectionString)
-                    dataTable.Rows(i).Item("QuoLocationCode") = QuoLocationCode
-                Next i
 
+                For i = 0 To dataSet.Tables("SupplierList").Rows.Count - 1
+                    If IsDBNull(dataSet.Tables("SupplierList").Rows(i).Item("IrregularSupplierCode")) Then
+                        dataSet.Tables("SupplierList").Rows(i).Item("QuoLocationCode") = dataSet.Tables("SupplierList").Rows(i).Item("DefaultQuoLocationCode")
+                    Else
+                        dataSet.Tables("SupplierList").Rows(i).Item("QuoLocationCode") = dataSet.Tables("SupplierList").Rows(i).Item("IrregularQuoLocationCode")
+                    End If
+
+                    If IsDBNull(dataSet.Tables("SupplierList").Rows(i).Item("QuoLocationCode")) Then
+                        dataSet.Tables("SupplierList").Rows(i).Item("QuoLocationCode") = "Direct"
+                    End If
+                Next i
             End Using
         Catch ex As Exception
-            st_Errorr_Meggage = "データベース接続時にエラーが発生しました。"
+            st_Errorr_Meggage = "Database error"
         End Try
 
         Return dataSet
 
     End Function
-
-    ' 見積回答拠点コード取得関数
-    ' 見積拠点例外テーブルを仕入先コードとロケーションで検索し、レコードが取得できれば見積回答拠点コードを返す。
-    ' 取得できなかった場合、国テーブルを仕入先の国コードで検索し、レコードを取得し、見積依頼先デフォルトを返す。
-    ' [パラメータ]
-    ' supplierCode As String: 仕入先コード
-    ' countryCode As String: 仕入先の国コード
-    ' connectionString As String) As String: 接続情報
-    '
-    ' [戻り値]
-    ' st_QuoLocationCode As String: 見積回答拠点コード（もしくは見積依頼先デフォルト）
-    Private Function Get_Quo_Location_Code(ByVal supplierCode As String, ByVal countryCode As String, ByVal connectionString As String) As String
-
-        Dim st_query As String
-        Dim st_QuoLocationCode As String = ""
-
-        Try
-
-            Using connection As New SqlClient.SqlConnection(connectionString)
-
-                st_query = "SELECT [QuoLocationCode] FROM [IrregularRFQLocation] WHERE [SupplierCode] = @Code AND [EnqLocationCode] = @Location"
-                Dim command As New SqlClient.SqlCommand(st_query, connection)
-                command.Parameters.AddWithValue("Code", supplierCode)
-                command.Parameters.AddWithValue("Location", st_Location) ' Public 変数
-                connection.Open()
-
-                Dim reader As SqlClient.SqlDataReader
-
-                reader = command.ExecuteReader()
-
-                If reader.HasRows Then
-                    reader.Read()
-                    st_QuoLocationCode = IIf(IsDBNull(reader.Item("QuoLocationCode")), "Direct", reader.Item("QuoLocationCode"))
-                End If
-                reader.Close()
-            End Using
-
-            If st_QuoLocationCode = "" Then
-
-                Using connection As New SqlClient.SqlConnection(connectionString)
-
-                    st_query = "SELECT [DefaultQuoLocationCode] FROM [PurchasingCountry] WHERE [CountryCode] = @CountryCode"
-                    Dim command As New SqlClient.SqlCommand(st_query, connection)
-                    command.Parameters.AddWithValue("CountryCode", countryCode)
-                    connection.Open()
-
-                    Dim reader As SqlClient.SqlDataReader
-
-                    reader = command.ExecuteReader()
-
-                    If reader.HasRows Then
-                        reader.Read()
-                        st_QuoLocationCode = IIf(IsDBNull(reader.Item("DefaultQuoLocationCode")), "Direct", reader.Item("DefaultQuoLocationCode"))
-                    Else
-                        st_Errorr_Meggage = "見積依頼先デフォルトが取得できませんでした。"
-                    End If
-                    reader.Close()
-                End Using
-            End If
-
-        Catch ex As Exception
-            st_Errorr_Meggage = "データベース接続時にエラーが発生しました。"
-        End Try
-
-        Return st_QuoLocationCode
-
-    End Function
-
 
 End Class
