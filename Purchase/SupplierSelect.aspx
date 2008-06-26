1a6e192e0fac99d9592cab453f0776a16e5b50a3
akutsu <akutsu@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-06-26 08:01:54 +0000

    MakerSelect からコピーした SupplierSelect を追加。
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@145 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

---
+++ Purchase/SupplierSelect.aspx
@@ -0,0 +1,89 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="SupplierSelect.aspx.vb" Inherits="Purchase.SupplierSelect" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Purchase DB</title>
+    <link rel="stylesheet" href="./CSS/Style.css" type="text/css" media="screen,print" />
+    <script type="text/javascript" src="./JS/Common.js"></script>
+    <script type="text/javascript" src="./JS/Colorful.js"></script>
+    <script type="text/javascript">
+    <!--
+
+window.onload = function() {
+   colorful.set();
+   changeCellColor("SupplierList_itemPlaceholderContainer")
+   
+}
+    function returnValues(code, name3, name4){
+      if(opener){
+        var name = name3 + " " + name4;
+        if (name3 == "") name = name4;
+        opener.document.getElementById('SupplierCode').value=code
+        opener.document.getElementById('SupplierName').value=name
+       }
+    }
+
+    -->
+    </script>
+</head>
+<body>
+	<!-- Main Content Area -->
+	<div id="content">
+		<div class="tabs"></div>
+
+		<h3>Supplier Select</h3>
+
+		<div class="main">
+			<p class="attention"><asp:Label ID="ErrorMessages" runat="server"></asp:Label></p>
+			<form id="SearchForm" runat="server">
+				<table>
+					<tr>
+						<th>Supplier Code : </th>
+						<td><asp:TextBox ID="Code" runat="server" Width="7em" MaxLength="5"></asp:TextBox></td>
+					</tr>
+					<tr>
+						<th>Supplier Name : </th>
+						<td><asp:TextBox ID="Name" runat="server" Width="21em" MaxLength="255"></asp:TextBox> (Partial text match)</td>
+					</tr>
+				</table>
+
+				<asp:Button ID="Search" runat="server" Text="Search" />
+				<input type="button" value="Clear" onclick="clearForm('SearchForm')" />
+			</form>
+		</div>
+
+		<hr />
+
+        <div class="list">
+            <asp:ListView ID="SupplierList" runat="server" AutoGenerateColumns="False">
+                <LayoutTemplate>
+                    <table ID="itemPlaceholderContainer" runat="server">
+                        <tr id="Tr1" runat="server">
+                            <th id="Th1" runat="server" style="width:15%">Supplier Code</th>
+                            <th id="Th2" runat="server" style="width:70%">Supplier Name</th>
+                        </tr>
+                        <tr ID="itemPlaceholder" runat="server">
+                        </tr>
+                    </table>
+                </LayoutTemplate>
+                <EmptyDataTemplate>
+                    <h3 style="font-style:italic">No match found.</h3>
+                </EmptyDataTemplate>
+                <ItemTemplate>
+                    <tr onclick="returnValues('<%#Eval("CountryCode")%>','<%#Eval("Name3")%>','<%#Eval("Name4")%>');">
+                        <td><asp:Label ID="SupplierCode" runat="server" Text='<%#Eval("SupplierCode")%>' /></td>
+                        <td>
+                          <asp:Label ID="SupplierName3" runat="server" Text='<%#Eval("Name3")%> ' />
+                          <asp:Label ID="SupplierName4" runat="server" Text='<%#Eval("Name4")%>' />
+                        </td>
+                    </tr>
+                </ItemTemplate>
+            </asp:ListView>
+        </div>
+    </div><!-- Main Content Area END -->
+
+
+</body>
+</html>
---
+++ Purchase/SupplierSelect.aspx.vb
@@ -0,0 +1,108 @@
+﻿Partial Public Class SupplierSelect
+    Inherits CommonPage
+
+    ' 接続文字列
+    Public DBConnectString As ConnectionStringSettings = ConfigurationManager.ConnectionStrings("DatabaseConnect")
+    Public st_Code As String
+    Public st_Name As String
+    Public st_Errorr_Meggage As String = ""
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        ' パラメータ Code, Name を取得
+        st_Code = IIf(Not (IsPostBack) And Not (String.IsNullOrEmpty(Request.QueryString("Code"))), Request.QueryString("Code"), Request.Form("Code"))
+        st_Name = Request.Form("Name")
+
+        ' 空白除去
+        st_Code = Trim(st_Code)
+        st_Name = Trim(st_Name)
+
+        ' 全角を半角に変換
+        st_Code = StrConv(st_Code, VbStrConv.Narrow)
+
+        ' 検索ブロックの TextBox の値を書き換え
+        Code.Text = st_Code
+        Name.Text = st_Name
+
+        ' Supplier List のデータをクリア
+        SupplierList.Items.Clear()
+        SupplierList.DataBind()
+
+        ' ポストバックではない 且つ GET が 空, notiong, NULL 以外なら実行
+        If Not (IsPostBack) And Not (String.IsNullOrEmpty(Request.QueryString("Code"))) Then
+            Dim dataSet As DataSet = New DataSet("Supplier")
+            dataSet = Get_Supplier_Data(dataSet, DBConnectString.ConnectionString)
+            SupplierList.DataSource = dataSet.Tables("SupplierList")
+            SupplierList.DataBind()
+        End If
+
+    End Sub
+
+    ' Search ボタンクリック処理
+    Protected Sub Search_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Search.Click
+        ' st_Code と st_Name があれば実行
+        If Not String.IsNullOrEmpty(st_Code) Or Not String.IsNullOrEmpty(st_Name) Then
+            Dim dataSet As DataSet = New DataSet("Supplier")
+            dataSet = Get_Supplier_Data(dataSet, DBConnectString.ConnectionString)
+            SupplierList.DataSource = dataSet.Tables("SupplierList")
+            SupplierList.DataBind()
+        End If
+    End Sub
+
+    ' 仕入先リスト取得関数
+    ' Public 変数の st_Code と st_Name を元に Supplier テーブルからデータを取得する。
+    '
+    ' [パラメータ]
+    ' dataSet 取得したデータをセットする DataSet オブジェクト
+    ' connectionString: 接続情報。文字列。
+    '
+    ' [戻り値]
+    ' dataSet: 取得したデータをセットした DataSet オブジェクト
+
+    Public Function Get_Supplier_Data(ByVal dataSet As DataSet, ByVal connectionString As String) As DataSet
+
+        ' WHERE 分の分岐
+        Dim st_where As String = " WHERE(SupplierCode = @Code) AND (ISNULL(Name1,'') + ' ' + ISNULL(Name2,'') LIKE N'%' + @Name + '%') "
+        If Not String.IsNullOrEmpty(st_Code) And String.IsNullOrEmpty(st_Name) Then
+            st_where = " WHERE(SupplierCode = @Code) "
+        ElseIf String.IsNullOrEmpty(st_Code) And Not String.IsNullOrEmpty(st_Name) Then
+            st_where = "  WHERE(ISNULL(Name1,'') + ' ' + ISNULL(Name2,'') LIKE N'%' + @Name + '%')  "
+        End If
+
+        ' 仕入先リスト取得
+        Dim st_query As String = _
+  " SELECT SupplierCode, Name3, Name4, CountryCode " _
+& " FROM  Supplier " _
+& st_where _
+& " ORDER BY SupplierCode, Name3 "
+
+        Try
+            Using connection As New SqlClient.SqlConnection(connectionString)
+
+                ' 接続情報、アダプタ、SQLコマンド オブジェクトの生成
+                Dim adapter As New SqlClient.SqlDataAdapter()
+                Dim command As New SqlClient.SqlCommand(st_query, connection)
+
+                ' DataSet にテーブルとカラムを追加
+                dataSet.Tables.Add("SupplierList")
+                dataSet.Tables("SupplierList").Columns.Add("QuoLocationCode", Type.GetType("System.String"))
+
+                ' SQL SELECT パラメータの追加
+                command.Parameters.AddWithValue("Code", st_Code)
+                command.Parameters.AddWithValue("Name", st_Name)
+
+                ' データベースからデータを取得
+                adapter.SelectCommand = command
+                adapter.Fill(dataSet.Tables("SupplierList"))
+
+            End Using
+        Catch ex As Exception
+            'Exception をスローする
+            Throw
+        End Try
+
+        Return dataSet
+
+    End Function
+
+End Class
