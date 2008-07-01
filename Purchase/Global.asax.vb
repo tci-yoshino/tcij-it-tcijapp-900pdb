fa29c977fb19c4c2d04b20e454c6ed710964a297
koyama <koyama@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-07-01 01:36:57 +0000

    Application_Error (共通エラー処理) を実装。 例外を担当者にメールで通知し、制御を SystemError.aspx に移す。
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@160 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/Global.asax.vb
+++ Purchase/Global.asax.vb
@@ -1,4 +1,6 @@
 ﻿Imports System.Web.SessionState
+Imports System.IO
+Imports System.Net.Mail
 
 Public Class Global_asax
     Inherits System.Web.HttpApplication
@@ -21,6 +23,33 @@ Public Class Global_asax
 
     Sub Application_Error(ByVal sender As Object, ByVal e As EventArgs)
         ' エラーの発生時に呼び出されます。
+        Dim context As HttpContext = DirectCast(sender, HttpApplication).Context
+        Dim ex As Exception = HttpContext.Current.Server.GetLastError
+
+        If TypeOf ex Is HttpUnhandledException AndAlso _
+            ex.InnerException IsNot Nothing Then
+            ex = ex.InnerException
+        End If
+
+        If ex IsNot Nothing Then
+            Try
+                Dim mail As New MailMessage
+                Dim appSetting As New System.Configuration.AppSettingsReader()
+
+                mail.To.Add(New MailAddress(appSetting.GetValue("ErrorMailTo", GetType(String))))
+                mail.Subject = "[Purchase DB] Internal System Error"
+                mail.Body = String.Format("An unhandled exception occurred: {1}{0}{0}Message: {2}{0}{0}Stack Trace:{0}{3}{0}{0}User Agent:{0}{4}", _
+                    System.Environment.NewLine, context.Request.RawUrl, ex.Message, ex.StackTrace, context.Request.UserAgent)
+                mail.IsBodyHtml = False
+
+                Dim smtp As New SmtpClient
+                smtp.Send(mail)
+            Catch
+                ' メール送信に失敗したときのエラーは捕捉しない
+            End Try
+        End If
+
+        Server.Transfer("./SystemError.aspx")
     End Sub
 
     Sub Session_End(ByVal sender As Object, ByVal e As EventArgs)
