70f22fac8665c5921aa46baf8255117a1e8a2847
ichihashi <ichihashi@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-07-03 09:40:16 +0000

    PostBackUrlのプログラム側での制御テスト。うまくいったのでこのまま使用する。＆今日はここまで。市橋。
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@191 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/RFQIssue.aspx.vb
+++ Purchase/RFQIssue.aspx.vb
@@ -2,7 +2,25 @@
     Inherits CommonPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim a As String = ""
+        If IsPostBack = True Then
+
+            If Request.QueryString("Action") = "Issue" Then
+
+            Else
+
+            End If
+        Else
+
+        End If
+    End Sub
+
+    Protected Sub EnqLocation_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs) Handles EnqLocation.SelectedIndexChanged
+        'ドロップダウンリストの項目を入れ替える。
 
     End Sub
 
+    Private Sub Page_PreRender(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.PreRender
+        Issue.PostBackUrl = "~/RFQIssue.aspx?Action=Issue"
+    End Sub
 End Class
