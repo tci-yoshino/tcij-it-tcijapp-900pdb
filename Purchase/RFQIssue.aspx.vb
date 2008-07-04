ba378a214af4601e26c9b2837ea0440fe735a25f
ichihashi <ichihashi@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-07-04 10:15:27 +0000

    Page_PreRender処理作成。市橋。
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@198 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/RFQIssue.aspx.vb
+++ Purchase/RFQIssue.aspx.vb
@@ -21,6 +21,18 @@
     End Sub
 
     Private Sub Page_PreRender(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.PreRender
-        Issue.PostBackUrl = "~/RFQIssue.aspx?Action=Issue"
+        If IsPostBack = False Then
+            Issue.PostBackUrl = "~/RFQIssue.aspx?Action=Issue"
+
+        End If
+        If Request.QueryString("ProductID").Length > 0 Then
+
+        End If
+        'パラメータ ProductID を受け取った場合
+        'テキストボックス ProductNumber，ProductName を ReadOnly="true" CssClass="readonly" ProductNumber 横の虫眼鏡は非表示にする。 
+        'パラメータ SupplierCode が渡されたとき
+        'テキストボックス SupplierCode，R3SupplierCode，SupplierName，SupplierCountry を ReadOnly="true" CssClass="readonly" SupplierCode 横の虫眼鏡は非表示にする。 
+        'それぞれのパラメータが渡されない場合は ReadOnly CssClass の指定は変更しない。 
+
     End Sub
 End Class
