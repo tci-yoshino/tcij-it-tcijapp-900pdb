9b03f5c49fc9ff845ca9d2fd5796a2ab350c44bd
koyama <koyama@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-07-08 09:49:05 +0000

    下記の定数を追加。 *LOCATION_JP 本社拠点コード *DATE_FORMAT 日付フォーマット *DATETIME_FORMAT 日付フォーマット (時刻つき)
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@204 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/Common.vb
+++ Purchase/Common.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Common
+    Public Shared ReadOnly LOCATION_JP As String = "JP"
 
+    Public Shared ReadOnly DATE_FORMAT As String = "yyyy-MM-dd"
+    Public Shared ReadOnly DATETIME_FORMAT As String = "yyyy-MM-dd HH:mm:ss"
 End Class
