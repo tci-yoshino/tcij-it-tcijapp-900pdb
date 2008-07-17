ef9ac3c17b24ca32416c3c14f8171f1ca7e3343a
koyama <koyama@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-07-17 10:06:31 +0000

    定数 DIRECT を追加。
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@237 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/Common.vb
+++ Purchase/Common.vb
@@ -1,6 +1,8 @@
 ﻿Public Class Common
     Public Shared ReadOnly LOCATION_JP As String = "JP"
 
+    Public Shared ReadOnly DIRECT As String = "Direct"
+
     Public Shared ReadOnly DATE_FORMAT As String = "yyyy-MM-dd"
     Public Shared ReadOnly DATETIME_FORMAT As String = "yyyy-MM-dd HH:mm:ss"
 End Class
