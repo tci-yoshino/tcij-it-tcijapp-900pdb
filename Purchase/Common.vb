f222a73718207c5dc7c61514df16002ab0998590
koyama <koyama@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-07-31 11:29:29 +0000

    エラーメッセージ用の定数を追加。 *ERR_INVALID_PARAMETER 不正なパラメータが渡されたとき *ERR_REQUIRED_FIELD 必須項目が未入力のとき *ERR_INCORRECT_FORMAT フォーマットが不正確なとき *ERR_INVALID_DATE 無効な日付のとき
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@260 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/Common.vb
+++ Purchase/Common.vb
@@ -8,4 +8,9 @@
 
     Public Shared ReadOnly DATE_REGEX As String = "\d{4}-\d{1,2}-\d{1,2}"
     Public Shared ReadOnly DATE_REGEX_OPTIONAL As String = "\d{4}-\d{1,2}-\d{1,2}|"
+
+    Public Shared ReadOnly ERR_INVALID_PARAMETER As String = "SYSTEM ERROR: Invalid parameter supplied."
+    Public Shared ReadOnly ERR_REQUIRED_FIELD As String = " must be specified."
+    Public Shared ReadOnly ERR_INCORRECT_FORMAT As String = " is not in the correct format."
+    Public Shared ReadOnly ERR_INVALID_DATE As String = " is an invalid date."
 End Class
