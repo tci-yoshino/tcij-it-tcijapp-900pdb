6f7abc2d077f5024abff3b5308b9bb1dcd19171f
koyama <koyama@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-07-31 11:26:08 +0000

    下記の定数を追加。 *DATE_REGEX 日付フォーマット正規表現 *DATE_REGEX_OPTIONAL 日付フォーマット正規表現 (任意)
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@259 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/Common.vb
+++ Purchase/Common.vb
@@ -5,4 +5,7 @@
 
     Public Shared ReadOnly DATE_FORMAT As String = "yyyy-MM-dd"
     Public Shared ReadOnly DATETIME_FORMAT As String = "yyyy-MM-dd HH:mm:ss"
+
+    Public Shared ReadOnly DATE_REGEX As String = "\d{4}-\d{1,2}-\d{1,2}"
+    Public Shared ReadOnly DATE_REGEX_OPTIONAL As String = "\d{4}-\d{1,2}-\d{1,2}|"
 End Class
