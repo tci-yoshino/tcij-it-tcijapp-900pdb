e4e757aa342a0f9841c3f73996819ddb77abec49
akutsu <akutsu@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-08-01 04:56:23 +0000

    クエリ修正。
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@267 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/RequestedTask.aspx.vb
+++ Purchase/RequestedTask.aspx.vb
@@ -2,11 +2,13 @@
     Inherits CommonPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        SrcRFQ.SelectCommand = "SELECT RH.RFQNumber, RH.StatusChangeDate, RH.Status, RH.ProductNumber, RH.ProductName, RH.Purpose, RH.QuoUserName, RH.QuoLocationName, RH.SupplierName, RH.MakerName, RR.RFQCorres " _
-& "FROM v_RFQHeader AS RH LEFT OUTER JOIN " _
-& "     v_RFQReminder AS RR ON RH.RFQNumber = RR.RFQNumber AND RR.RcptUserID = '" & Session("UserID") & "' " _
-& "WHERE EnqUserID = '" & Session("UserID") & "' " _
-& "ORDER BY StatusSortOrder, StatusChangeDate "
+        SrcRFQ.SelectCommand = _
+              "SELECT RH.RFQNumber, RH.StatusChangeDate, RH.Status, RH.ProductNumber, RH.ProductName, RH.Purpose, " _
+            & "       RH.QuoUserName, RH.QuoLocationName, RH.SupplierName, RH.MakerName, RR.RFQCorres AS RFQCorrespondence " _
+            & "FROM v_RFQHeader AS RH LEFT OUTER JOIN " _
+            & "     v_RFQReminder AS RR ON RH.RFQNumber = RR.RFQNumber AND RR.RcptUserID = '" & Session("UserID") & "' " _
+            & "WHERE EnqUserID = '" & Session("UserID") & "' " _
+            & "ORDER BY StatusSortOrder, StatusChangeDate "
 
     End Sub
 
