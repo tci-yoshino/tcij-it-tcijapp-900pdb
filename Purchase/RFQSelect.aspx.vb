6857084a2b0600c823c3a2d0ad207a3936a827fc
akutsu <akutsu@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-08-18 01:40:30 +0000

    構造体を無くし、各stringオブジェクトとして定義した。 NEXT ボタンクリック処理を削除。
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@344 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/RFQSelect.aspx.vb
+++ Purchase/RFQSelect.aspx.vb
@@ -2,25 +2,15 @@
     Inherits CommonPage
     ' 変数宣言
     Private DBConnectString As ConnectionStringSettings = ConfigurationManager.ConnectionStrings("DatabaseConnect")
+    Protected st_ParPONumber As String = "" ' aspx 側で読むため、Protected にする
+    Private st_ProductID As String = ""
+    Private st_SupplierCode As String = ""
+    Private st_MakerCode As String = ""
+
     ' 定数宣言
     Private Const MSG_REQUIED_ParPONumber = "購買発注番号が指定されていません。"
-    Private Const MSG_REQUIED_ACTION = "データを処理できませんでした。"
-    ' 検索キー用構造体(親POのデータが入る）
-    Structure ParPOData
-        Dim PONumber As String
-        Dim ProductID As String
-        Dim SupplierCode As String
-        Dim MakerCode As String
-    End Structure
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
-        ' 変数宣言
-        Dim PPO As ParPOData
-        PPO.PONumber = ""
-        PPO.ProductID = ""
-        PPO.SupplierCode = ""
-        PPO.MakerCode = ""
 
         ' コントロール初期化
         Msg.Text = ""
@@ -29,45 +19,46 @@
 
         ' パラメータ取得
         If Request.RequestType = "POST" Then
-            PPO.PONumber = IIf(String.IsNullOrEmpty(Request.Form("ParPONumber")), "", Request.Form("ParPONumber"))
+            st_ParPONumber = IIf(String.IsNullOrEmpty(Request.Form("ParPONumber")), "", Request.Form("ParPONumber"))
         ElseIf Request.RequestType = "GET" Then
-            PPO.PONumber = IIf(String.IsNullOrEmpty(Request.QueryString("ParPONumber")), "", Request.QueryString("ParPONumber"))
+            st_ParPONumber = IIf(String.IsNullOrEmpty(Request.QueryString("ParPONumber")), "", Request.QueryString("ParPONumber"))
         End If
 
         ' 空白除去
-        PPO.PONumber = PPO.PONumber.Trim
+        st_ParPONumber = st_ParPONumber.Trim
 
         ' HiddenField に設定
-        ParPONumber.Value = PPO.PONumber
+        ParPONumber.Value = st_ParPONumber
+        st_ParPONumber = st_ParPONumber
 
         ' パラメータを取得できなかった場合はエラー終了
-        If String.IsNullOrEmpty(PPO.PONumber) Then
+        If String.IsNullOrEmpty(st_ParPONumber) Then
             Msg.Text = MSG_REQUIED_ParPONumber
             SelectForm.Visible = False
             Exit Sub
         End If
 
         ' 親データ取得。取得できなかった場合はエラー終了
-        SetParPOData(PPO)
-        If String.IsNullOrEmpty(PPO.SupplierCode) Then
+        Set_ParPOData(st_ParPONumber, st_ProductID, st_SupplierCode, st_MakerCode)
+        If String.IsNullOrEmpty(st_SupplierCode) Then
             Msg.Text = Common.ERR_NO_MATCH_FOUND
             SelectForm.Visible = False
             Exit Sub
         End If
 
         ' 製品情報取得 & Label にセット
-        SetProductData(PPO.ProductID)
+        Set_ProductData(st_ProductID)
 
         ' 仕入先情報取得 & Label にセット
-        SetSupplierData(PPO.SupplierCode)
+        Set_SupplierData(st_SupplierCode)
 
         ' RFQHeader 取得 & バインド
-        SetRFQHeaderQuery(PPO)
+        Set_RFQHeaderQuery(st_ProductID, st_SupplierCode, st_MakerCode)
     End Sub
 
     ' 親POデータを取得する
     ' ParPONumber をキーに PO を検索し、ProductID、SupplierCode、MaKerCode に値をセットする。
-    Protected Sub SetParPOData(ByRef data As ParPOData)
+    Protected Sub Set_ParPOData(ByVal PONumber As String, ByRef ProductID As String, ByRef SupplierCode As String, ByRef MakerCode As String)
         Try
             Using connection As New SqlClient.SqlConnection(DBConnectString.ConnectionString)
                 Dim st_query As String = "SELECT ProductID, SupplierCode, MakerCode FROM PO WHERE PONumber = @PONumber"
@@ -75,7 +66,7 @@
                 connection.Open()
 
                 ' SQL SELECT パラメータの追加
-                command.Parameters.AddWithValue("PONumber", data.PONumber)
+                command.Parameters.AddWithValue("PONumber", PONumber)
 
                 ' SqlDataReader を生成し、検索処理を実行。
                 Dim reader As SqlClient.SqlDataReader = command.ExecuteReader()
@@ -83,9 +74,9 @@
                 ' データを変数にセット
                 If reader.HasRows Then
                     reader.Read()
-                    data.ProductID = IIf(IsDBNull(reader("ProductID")), "", reader("ProductID"))
-                    data.SupplierCode = IIf(IsDBNull(reader("SupplierCode")), "", reader("SupplierCode"))
-                    data.MakerCode = IIf(IsDBNull(reader("MakerCode")), "", reader("MakerCode"))
+                    ProductID = IIf(IsDBNull(reader("ProductID")), "", reader("ProductID"))
+                    SupplierCode = IIf(IsDBNull(reader("SupplierCode")), "", reader("SupplierCode"))
+                    MakerCode = IIf(IsDBNull(reader("MakerCode")), "", reader("MakerCode"))
                 End If
 
                 reader.Close()
@@ -96,7 +87,7 @@
     End Sub
 
     ' 製品情報を取得し、aspx のラベルにセットする
-    Protected Sub SetProductData(ByVal ProductID As String)
+    Protected Sub Set_ProductData(ByVal ProductID As String)
         Try
             Using connection As New SqlClient.SqlConnection(DBConnectString.ConnectionString)
                 Dim st_query As String = _
@@ -128,7 +119,7 @@
     End Sub
 
     ' 仕入先情報取得し、aspx のラベルにセットする
-    Protected Sub SetSupplierData(ByVal SuppplierCode As String)
+    Protected Sub Set_SupplierData(ByVal SuppplierCode As String)
         Try
             Using connection As New SqlClient.SqlConnection(DBConnectString.ConnectionString)
                 Dim st_query As String = _
@@ -166,14 +157,14 @@
     End Sub
 
     ' RFQHeader を取得するためのクエリを SQL データソースコントロールに設定する
-    Protected Sub SetRFQHeaderQuery(ByVal data As ParPOData)
+    Protected Sub Set_RFQHeaderQuery(ByVal ProductID As String, ByVal SupplierCode As String, ByVal MakerCode As String)
 
         SrcRFQHeader.SelectParameters.Clear()
-        SrcRFQHeader.SelectParameters.Add("ProductID", data.ProductID)
-        SrcRFQHeader.SelectParameters.Add("SuplierCode", data.SupplierCode)
+        SrcRFQHeader.SelectParameters.Add("ProductID", ProductID)
+        SrcRFQHeader.SelectParameters.Add("SuplierCode", SupplierCode)
         Dim st_where As String = ""
-        If Not String.IsNullOrEmpty(data.MakerCode) Then
-            SrcRFQHeader.SelectParameters.Add("MakerCode", data.MakerCode)
+        If Not String.IsNullOrEmpty(MakerCode) Then
+            SrcRFQHeader.SelectParameters.Add("MakerCode", MakerCode)
             st_where = " AND MakerCode = @MakerCode "
         End If
 
@@ -201,11 +192,11 @@
 
     End Sub
 
-    ' RFQLine を RFQHeader のレコードごとに取得し、バインドする
-    Protected Sub Get_RFQLine(ByVal sender As Object, ByVal e As System.EventArgs) Handles RFQHeaderList.ItemDataBound
-        Dim lv As ListView = CType(CType(e, ListViewItemEventArgs).Item.FindControl("RFQLineList"), ListView)
-        Dim src As SqlDataSource = CType(CType(e, ListViewItemEventArgs).Item.FindControl("SrcRFQLine"), SqlDataSource)
-        Dim label As Label = CType(CType(e, ListViewItemEventArgs).Item.FindControl("RFQNumber"), Label)
+    ' RFQLine を RFQHeader のレコードごとに取得する。
+    Protected Sub Set_RFQLine(ByVal sender As Object, ByVal e As System.EventArgs) Handles RFQHeaderList.ItemDataBound
+
+        Dim src As SqlDataSource = DirectCast(DirectCast(e, ListViewItemEventArgs).Item.FindControl("SrcRFQLine"), SqlDataSource)
+        Dim label As Label = DirectCast(DirectCast(e, ListViewItemEventArgs).Item.FindControl("RFQNumber"), Label)
 
         src.SelectParameters.Clear()
         src.SelectParameters.Add("RFQNumber", label.Text)
@@ -214,46 +205,6 @@
             & "  QuoPer, QuoUnitCode, LeadTime, Packing, Purity, QMMethod " _
             & "FROM v_RFQLine " _
             & "WHERE RFQNumber = @RFQNumber "
-        'lv.DataSourceID = src.ID
-        'lv.DataBind()
-    End Sub
-
-    ' NextPage ボタンクリック処理
-    Protected Sub Next_Click(ByVal sender As Object, ByVal e As System.EventArgs)
-        Dim st_Action As String = "" ' Action 格納変数
-        Dim st_ParPONumber As String = ""
-        Dim st_RFQLineData() As String ' LineNumber と UnitPrice を格納
-
-        ' パラメータ取得
-        If Request.RequestType = "POST" Then
-            st_Action = IIf(Request.Form("Action") = Nothing, "", Request.Form("Action"))
-            st_ParPONumber = IIf(Request.Form("ParPONumber") = Nothing, "", Request.Form("ParPONumber"))
-        ElseIf Request.RequestType = "GET" Then
-            st_Action = IIf(Request.QueryString("Action") = Nothing, "", Request.QueryString("Action"))
-            st_ParPONumber = IIf(Request.QueryString("ParPONumber") = Nothing, "", Request.QueryString("ParPONumber"))
-        End If
-
-        st_RFQLineData = Split(CType(sender, Button).CommandArgument, ",")
-
-        ' パラメータチェック
-        ' 取得できない・"Next" で無い場合はエラー
-        If String.IsNullOrEmpty(st_action) Or st_action <> "Next" Then
-            Msg.Text = MSG_REQUIED_ACTION
-            Exit Sub
-        End If
-        'ParPONumber が取得できない場合はエラー
-        If String.IsNullOrEmpty(st_ParPONumber) Then
-            Msg.Text = MSG_REQUIED_ACTION
-            Exit Sub
-        End If
-        ' RFQLine のデータが取得できない場合はエラー
-        If String.IsNullOrEmpty(st_RFQLineData(0)) Or String.IsNullOrEmpty(st_RFQLineData(1)) Then
-            Msg.Text = MSG_REQUIED_ACTION
-            Exit Sub
-        End If
-
-        ' POIssue.aspx に遷移
-        Response.Redirect("POIssue.aspx?ParPONumber=" & st_ParPONumber & "&RFQLineNumber=" & st_RFQLineData(0))
 
     End Sub
 
