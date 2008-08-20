d470f8a7bb9a22c420cf7d3d8028e34b51c4b843
akutsu <akutsu@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-08-20 08:56:02 +0000

    Assign_Click の処理をRFQ と PO で分けました。 その他、変数宣言などの細かい処理を修正。
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@380 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/UnassignedTask.aspx.vb
+++ Purchase/UnassignedTask.aspx.vb
@@ -1,19 +1,21 @@
 ﻿Partial Public Class UnassignedTask
     Inherits CommonPage
 
-    Public DBConnectString As ConnectionStringSettings = ConfigurationManager.ConnectionStrings("DatabaseConnect")
+    Private DBConnectString As New SqlClient.SqlConnection(Common.DB_CONNECT_STRING)
+    Const ERR_UPDATE As String = "既に他のユーザによって更新されました。"
+    Const ASSIGN_ACTION As String = "Assign"
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
-        ErrorMessages.Text = ""
+        Msg.Text = ""
 
         SrcRFQ.SelectCommand = _
               "SELECT VRH.RFQNumber, VRH.StatusChangeDate, VRH.Status, VRH.ProductNumber, VRH.ProductName, " _
             & "       VRH.Purpose, VRH.EnqUserName, VRH.EnqLocationName, VRH.SupplierName, VRH.MakerName, " _
-            & "       CONVERT(VARCHAR, RH.UpdateDate, 120) AS UpdateDate " _
-            & "FROM v_RFQHeader AS VRH, RFQHeader AS RH " _
-            & "WHERE VRH.RFQNumber = RH.RFQNumber " _
-            & "  AND VRH.QuoLocationCode = '" & Session("LocationCode") & "' " _
+            & "       CONVERT(VARCHAR, VRH.UpdateDate, 120) AS UpdateDate " _
+            & "FROM v_RFQHeader AS VRH " _
+            & "WHERE " _
+            & "  VRH.QuoLocationCode = '" & Session("LocationCode") & "' " _
             & "  AND VRH.QuoUserID IS NULL " _
             & "ORDER BY VRH.RFQNumber "
 
@@ -21,21 +23,67 @@
               "SELECT VP.PONumber, VP.PODate, VP.POLocationCode, VP.POLocationName, VP.POUserID, VP.POUserName, " _
             & "       VP.ProductNumber, VP.ProductName, VP.SupplierCode, VP.SupplierName, " _
             & "       VP.MakerCode, VP.MakerName, VP.ParPONumber, VP.Status, VP.StatusChangeDate, " _
-            & "       CONVERT(VARCHAR, P.UpdateDate, 120) AS UpdateDate " _
-            & "FROM v_PO AS VP ,PO AS P " _
-            & "WHERE VP.PONumber = P.PONumber " _
-            & "  AND VP.SOLocationCode = '" & Session("LocationCode") & "' " _
+            & "       CONVERT(VARCHAR, VP.UpdateDate, 120) AS UpdateDate " _
+            & "FROM v_PO AS VP " _
+            & "WHERE " _
+            & "  VP.SOLocationCode = '" & Session("LocationCode") & "' " _
             & "  AND VP.SOUserID IS NULL " _
             & "ORDER BY VP.PONumber "
 
 
-        SrcQuo.SelectCommand = "SELECT UserID, Name FROM v_User WHERE LocationCode = '" & Session("LocationCode") & "' "
+        SrcUser.SelectCommand = "SELECT UserID, Name FROM v_User WHERE isDisabled = 0 AND LocationCode = '" & Session("LocationCode") & "' "
+
     End Sub
 
-    Protected Sub Assign_Click(ByVal source As Object, ByVal e As ListViewCommandEventArgs) Handles RFQList.ItemCommand, POList.ItemCommand
+    Protected Sub RFQAssign_Click(ByVal source As Object, ByVal e As ListViewCommandEventArgs) Handles RFQList.ItemCommand
 
         Dim st_action As String = "" ' Action 格納変数
-        Dim i_updatelen As Integer ' Update が実行された行数を格納する変数
+        Dim st_RFQNumber As String = CType(e.Item.FindControl("RFQNumber"), Label).Text
+        Dim st_UpdateDate As String = CType(e.Item.FindControl("UpdateDate"), HiddenField).Value
+        Dim st_QuoUserID As String = CType(e.Item.FindControl("QuoUser"), DropDownList).SelectedValue
+
+        ' Action 取得
+        If Request.RequestType = "POST" Then
+            st_action = IIf(Request.Form("Action") = Nothing, "", Request.Form("Action"))
+        ElseIf Request.RequestType = "GET" Then
+            st_action = IIf(Request.QueryString("Action") = Nothing, "", Request.QueryString("Action"))
+        End If
+
+        ' Action が "Assign"でない場合はエラー
+        If st_action <> ASSIGN_ACTION Then
+            Msg.Text = Common.ERR_INVALID_PARAMETER
+            RFQList.DataBind()
+            Exit Sub
+        End If
+
+        ' Update Chack
+        If Not Common.isLatestData("RFQHeader", "RFQNumber", st_RFQNumber, st_UpdateDate) Then
+            Msg.Text = ERR_UPDATE
+            RFQList.DataBind()
+            Exit Sub
+        End If
+
+        ' SQLパラメータ設定
+        SrcRFQ.UpdateParameters.Add("RFQNumber", st_RFQNumber)
+        SrcRFQ.UpdateParameters.Add("QuoUserID", st_QuoUserID)
+        SrcRFQ.UpdateParameters.Add("UpdatedBy", Session("UserID"))
+        SrcRFQ.UpdateParameters.Add("UpdateDate", st_UpdateDate)
+
+        ' Update 文作成
+        SrcRFQ.UpdateCommand = _
+              "UPDATE RFQHeader " _
+            & "SET QuoUserID = @QuoUserID, RFQStatusCode = 'A', UpdateDate = GETDATE(), UpdatedBy = @UpdatedBy " _
+            & "WHERE RFQNumber = @RFQNumber "
+        SrcRFQ.Update()
+
+    End Sub
+
+    Protected Sub POAssign_Click(ByVal source As Object, ByVal e As ListViewCommandEventArgs) Handles POList.ItemCommand
+
+        Dim st_action As String = "" ' Action 格納変数
+        Dim st_PONumber As String = CType(e.Item.FindControl("PONumber"), Label).Text
+        Dim st_SOUserID As String = CType(e.Item.FindControl("SOUser"), DropDownList).SelectedValue
+        Dim st_UpdateDate As String = CType(e.Item.FindControl("UpdateDate"), HiddenField).Value
 
         ' Action取得
         If Request.RequestType = "POST" Then
@@ -44,57 +92,32 @@
             st_action = IIf(Request.QueryString("Action") = Nothing, "", Request.QueryString("Action"))
         End If
 
-        ' Action が "Assign" なら実行
-        If st_action = "Assign" Then
-            ' 親コントローラの ID によって処理を分ける
-            If e.Item.Parent.NamingContainer.ID = "RFQList" Then
-
-                ' SQLパラメータ設定
-                SrcRFQ.UpdateParameters.Add("RFQNumber", CType(e.Item.FindControl("RFQNumber"), Label).Text)
-                SrcRFQ.UpdateParameters.Add("QuoUserID", CType(e.Item.FindControl("QuoUser"), DropDownList).SelectedValue)
-                SrcRFQ.UpdateParameters.Add("UpdatedBy", Session("UserID"))
-                SrcRFQ.UpdateParameters.Add("UpdateDate", CType(e.Item.FindControl("UpdateDate"), HiddenField).Value)
-
-                ' Update 文作成
-                SrcRFQ.UpdateCommand = _
-                      "UPDATE RFQHeader " _
-                    & "SET QuoUserID = @QuoUserID, RFQStatusCode = 'A', UpdateDate = GETDATE(), UpdatedBy = @UpdatedBy " _
-                    & "WHERE RFQNumber = @RFQNumber " _
-                    & "  AND CONVERT(VARCHAR, UpdateDate, 120) = @UpdateDate "
-
-                ' Update 実行、実行行数取得
-                i_updatelen = SrcRFQ.Update()
-                ' 再バインド
-                RFQList.DataBind()
-
-            ElseIf e.Item.Parent.NamingContainer.ID = "POList" Then
-
-                ' SQLパラメータ設定
-                SrcPO.UpdateParameters.Add("PONumber", CType(e.Item.FindControl("PONumber"), Label).Text)
-                SrcPO.UpdateParameters.Add("SOUserID", CType(e.Item.FindControl("SOUser"), DropDownList).SelectedValue)
-                SrcPO.UpdateParameters.Add("UpdatedBy", Session("UserID"))
-                SrcPO.UpdateParameters.Add("UpdateDate", CType(e.Item.FindControl("UpdateDate"), HiddenField).Value)
-
-                ' Update 文作成
-                SrcPO.UpdateCommand = _
-                      "UPDATE PO " _
-                    & "SET SOUserID = @SOUserID, UpdateDate = GETDATE(), UpdatedBy = @UpdatedBy " _
-                    & "WHERE PONumber = @PONumber " _
-                    & "  AND CONVERT(VARCHAR, UpdateDate, 120) = @UpdateDate "
-
-                ' Update 実行、実行行数取得
-                i_updatelen = SrcPO.Update()
-                ' 再バインド
-                POList.DataBind()
-            End If
-
-            ' Update された行数が 0 の場合はエラーメッセージ表示。
-            If i_updatelen = 0 Then
-                ErrorMessages.Text = "既に他のユーザによって更新されました。"
-            End If
+        ' Action が "Assign"でない場合はエラー
+        If st_action <> ASSIGN_ACTION Then
+            Msg.Text = Common.ERR_INVALID_PARAMETER
+            POList.DataBind()
+            Exit Sub
         End If
 
+        ' Update Chack
+        If Not Common.isLatestData("RFQHeader", "RFQNumber", st_PONumber, st_UpdateDate) Then
+            Msg.Text = ERR_UPDATE
+            POList.DataBind()
+            Exit Sub
+        End If
 
+        ' SQLパラメータ設定
+        SrcPO.UpdateParameters.Add("PONumber", st_PONumber)
+        SrcPO.UpdateParameters.Add("SOUserID", st_SOUserID)
+        SrcPO.UpdateParameters.Add("UpdatedBy", Session("UserID"))
+        SrcPO.UpdateParameters.Add("UpdateDate", st_UpdateDate)
+
+        ' Update 文作成
+        SrcPO.UpdateCommand = _
+              "UPDATE PO " _
+            & "SET SOUserID = @SOUserID, UpdateDate = GETDATE(), UpdatedBy = @UpdatedBy " _
+            & "WHERE PONumber = @PONumber "
+        SrcPO.Update()
 
     End Sub
 
