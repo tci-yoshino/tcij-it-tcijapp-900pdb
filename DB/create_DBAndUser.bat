6a42e6196f8fe29ed5182b4ab2e81c67577f61cb
akutsu <akutsu@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-08-29 09:14:25 +0000

    if 文削除。
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@515 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- DB/create_DBAndUser.bat
+++ DB/create_DBAndUser.bat
@@ -4,7 +4,4 @@ SET DBFileDir=
 SET/P DataBaseName="作成するデータベース名を入力してください: "
 SET/P DBFileDir="テーブルログディレクトリを入力してください: "
 
-if %DBFileDir% == "" (	
-  exit/b
-)
 sqlcmd -i "create_DBAndUser.sql" -o "create_DBAndUser.log"
