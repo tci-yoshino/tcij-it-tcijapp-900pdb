efa4800d081eb4ebbb00ad98d49723ca9e3d684c
toshiteru <toshiteru@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-09-01 05:09:10 +0000

    CountryListの表示をCountryName順にした
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@530 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/CountryList.aspx.vb
+++ Purchase/CountryList.aspx.vb
@@ -2,7 +2,7 @@
     Inherits CommonPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        SrcCountry.SelectCommand = "SELECT [CountryCode], [CountryName], [DefaultQuoLocationName], './CountrySetting.aspx?Action=Edit&Code=' + [CountryCode] AS Url FROM [v_Country]"
+        SrcCountry.SelectCommand = "SELECT [CountryCode], [CountryName], [DefaultQuoLocationName], './CountrySetting.aspx?Action=Edit&Code=' + [CountryCode] AS Url FROM [v_Country] ORDER BY CountryName"
     End Sub
 
 End Class
