1c892db6025c6cba7e1cb7a0a7eb0e1dc752ba76
toshiteru <toshiteru@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-09-01 02:50:14 +0000

    入力データのチェック強化
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@521 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/SupplierSearch.aspx.vb
+++ Purchase/SupplierSearch.aspx.vb
@@ -13,37 +13,45 @@
         '[Supplier検索]-------------------------------------------------------------------
         If Code.Text.ToString <> "" And Not IsNumeric(Code.Text.ToString) Then
             Msg.Text = "SupplierCodeには数字を入力して下さい"
-            SrcSupplier.SelectCommand = ""
-            SupplierList.DataBind()
-        Else
-            Msg.Text = ""
-            Dim SQLStr As String = ""
-            SrcSupplier.SelectCommand = "SELECT SupplierCode AS [Supplier Code], R3SupplierCode AS [R/3 Supplier Code], ISNULL(Name3, '') + N' ' + ISNULL(Name4, '') AS [Supplier Name], './SupplierSetting.aspx?Action=Edit&Code=' + rtrim(ltrim(str([SupplierCode]))) AS Url  FROM dbo.Supplier "
-            If Code.Text.ToString <> "" Then
-                If SQLStr = "" Then SQLStr = "WHERE "
-                SQLStr = SQLStr + "(SupplierCode = '" & Code.Text.ToString & "')"
-            End If
-            '[R3Codeが数字の場合と文字の場合とでは検索が異なる]---------------------------
-            If R3Code.Text.ToString <> "" Then
-                If SQLStr = "" Then SQLStr = "WHERE " Else SQLStr = SQLStr + " AND "
-                If IsNumeric(R3Code.Text.ToString) And R3Code.Text Like "*.*" = False Then
-                    SQLStr = SQLStr + "(R3SupplierCode = " & R3Code.Text.ToString & ")"
-                Else
-                    SQLStr = SQLStr + "(R3SupplierCode = '" & R3Code.Text.ToString & "')"
-                End If
-            End If
-            If Name.Text.ToString <> "" Then
-                If SQLStr = "" Then SQLStr = "WHERE " Else SQLStr = SQLStr + " AND "
-                SQLStr = SQLStr + "(Name1 + N' ' + Name2 LIKE '%" & Name.Text.ToString & "%')"
-            End If
+            ClearListView()
+            Exit Sub
+        ElseIf Code.Text Like "*.*" = True Then
+            Msg.Text = "SupplierCodeには整数を入力して下さい"
+            ClearListView()
+            Exit Sub
+        End If
 
-            '[検索項目すべて指定しない場合は結果無しとする]-------------------------------
-            If SQLStr = "" Then
-                SrcSupplier.SelectCommand = ""
+        Msg.Text = ""
+        Dim SQLStr As String = ""
+        SrcSupplier.SelectCommand = "SELECT SupplierCode AS [Supplier Code], R3SupplierCode AS [R/3 Supplier Code], ISNULL(Name3, '') + N' ' + ISNULL(Name4, '') AS [Supplier Name], './SupplierSetting.aspx?Action=Edit&Code=' + rtrim(ltrim(str([SupplierCode]))) AS Url  FROM dbo.Supplier "
+        If Code.Text.ToString <> "" Then
+            If SQLStr = "" Then SQLStr = "WHERE "
+            SQLStr = SQLStr + "(SupplierCode = '" + Common.SafeSqlLikeClauseLiteral(Code.Text) + "')"
+        End If
+        '[R3Codeが数字の場合と文字の場合とでは検索が異なる]---------------------------
+        If R3Code.Text.ToString <> "" Then
+            If SQLStr = "" Then SQLStr = "WHERE " Else SQLStr = SQLStr + " AND "
+            If IsNumeric(R3Code.Text.ToString) And R3Code.Text Like "*.*" = False Then
+                SQLStr = SQLStr + "(R3SupplierCode = " + Common.SafeSqlLikeClauseLiteral(R3Code.Text) + ")"
             Else
-                SrcSupplier.SelectCommand = SrcSupplier.SelectCommand + SQLStr
+                SQLStr = SQLStr + "(R3SupplierCode = '" + Common.SafeSqlLikeClauseLiteral(R3Code.Text) + "')"
             End If
+        End If
+        If Name.Text.ToString <> "" Then
+            If SQLStr = "" Then SQLStr = "WHERE " Else SQLStr = SQLStr + " AND "
+            SQLStr = SQLStr + "ISNULL(Name1,'') + N' ' + ISNULL(Name2,'') LIKE '%" + Common.SafeSqlLikeClauseLiteral(Name.Text) + "%'"
+        End If
+
+        '[検索項目すべて指定しない場合は結果無しとする]-------------------------------
+        If SQLStr = "" Then
+            SrcSupplier.SelectCommand = ""
+        Else
+            SrcSupplier.SelectCommand = SrcSupplier.SelectCommand + SQLStr
         End If
     End Sub
 
+    Public Sub ClearListView()
+        SrcSupplier.SelectCommand = ""
+        SupplierList.DataBind()
+    End Sub
 End Class
