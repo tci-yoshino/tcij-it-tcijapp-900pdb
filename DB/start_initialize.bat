883835d8323a8287dca77b7937283593db5b17f0
akutsu <akutsu@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2008-09-02 06:05:27 +0000

    bat ファイル削除。
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@567 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- DB/start_initialize.bat
+++
@@ -1,8 +0,0 @@
-echo off
-SET DataBaseName=
-SET ScliptPath=C:\tcijapp\Purchase\DB\
-SET/P DataBaseName="データベース名を入力してください: "
-
-if "%DataBaseName%"=="" exit /b
-
-sqlcmd -i "start_initialize.sql" -o "start_initialize.log"
