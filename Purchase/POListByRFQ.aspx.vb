875916cfccee44dff554b5ff7b7972022be30f5b
koyama <koyama@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2016-04-21 04:11:09 +0000

    refs #443 権限が WRITE_P または READ_P のユーザが、直接、極秘品対象のページにアクセスした場合は権限エラーとする
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@1497 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- Purchase/POListByRFQ.aspx.vb
+++ Purchase/POListByRFQ.aspx.vb
@@ -1,4 +1,7 @@
-﻿Public Partial Class POListByRFQ
+﻿Imports System.Data.SqlClient
+Imports Purchase.Common
+
+Partial Public Class POListByRFQ
     Inherits CommonPage
 
     Protected st_RFQLineNumber As String = String.Empty
@@ -25,6 +28,22 @@
             Exit Sub
         End If
 
+        '権限ロールに従い極秘品はエラーとする
+        If Session(SESSION_ROLE_CODE).ToString = ROLE_WRITE_P OrElse Session(SESSION_ROLE_CODE).ToString = ROLE_READ_P Then
+            Using sqlConn As SqlConnection = New SqlConnection(DB_CONNECT_STRING)
+                Using sqlCmd As SqlCommand = sqlConn.CreateCommand()
+
+                    sqlCmd.CommandText = "SELECT 1 FROM v_RFQLine WHERE isCONFIDENTIAL = 1 AND RFQLineNumber = @RFQLineNumber"
+                    sqlCmd.Parameters.AddWithValue("RFQLineNumber", st_RFQLineNumber)
+                    sqlConn.Open()
+                    Dim dr As SqlDataReader = sqlCmd.ExecuteReader
+                    If dr.Read = True Then
+                        Response.Redirect("AuthError.html")
+                    End If
+                End Using
+            End Using
+        End If
+
         ' PO データを取得する
         SrcPO.SelectParameters.Clear()
         SrcPO.SelectParameters.Add("RFQLineNumber", st_RFQLineNumber)
@@ -33,7 +52,7 @@
             & "  PONumber, StatusChangeDate, Status, ProductNumber, ProductName, " _
             & "  PODate, POUserName, POLocationName, SupplierName, MakerName, " _
             & "  DeliveryDate, OrderQuantity, OrderUnitCode, CurrencyCode, " _
-            & "  UnitPrice, PerQuantity, PerUnitCode " _
+            & "  UnitPrice, PerQuantity, PerUnitCode, isCONFIDENTIAL " _
             & "FROM " _
             & "  v_PO " _
             & "WHERE " _
