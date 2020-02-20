e9d6dadc3262638eb54796ab6dd33f284e59c419
koyama <koyama@ebf3080d-37d0-4b6c-8d7e-121e4dec3f32>
2020-02-20 05:13:19 +0000

    refs #1466 TCIJAPP PDB 改修対応 (ベンダー納品19)
    git-svn-id: svn+ssh://devteam/data/svn/tcijapp/Purchase/trunk@1808 ebf3080d-37d0-4b6c-8d7e-121e4dec3f32

--- DB/S4/3Update/3AlterPROCEDURESp_SupplierImport.sql
+++ DB/S4/3Update/3AlterPROCEDURESp_SupplierImport.sql
@@ -1,12 +1,13 @@
 USE [Purchase]
 GO
 
-/****** Object:  StoredProcedure [dbo].[sp_SupplierImport]    Script Date: 2019/12/26 19:02:15 ******/
+/****** Object:  StoredProcedure [dbo].[sp_SupplierImport]    Script Date: 2020/02/17 21:29:47 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
+
 
 ALTER PROCEDURE [dbo].[sp_SupplierImport] 
 @ErrNUMBER int output,
@@ -75,6 +76,8 @@ begin try
     DECLARE @SupMAD10 nvarchar(50)
     DECLARE @SupREM10 nvarchar(50)
 	DECLARE @ExtSupNo nvarchar(50)
+	DECLARE @SupplierCode  nvarchar(50)
+	DECLARE @S4SupplierCode nvarchar(50)
     DECLARE s_SupplierCursor CURSOR FOR
       SELECT
         SupNo, SupCut, SupCom, SupMei1, SupMei2, SupMei3, SupMei4,
@@ -93,9 +96,31 @@ begin try
 		SupID10,SupMAD10,SupREM10,
 		ExtSupNo
       FROM
-        Supplier JOIN s_Supplier ON Supplier.SupplierCode = s_Supplier.ExtSupNo
+		Supplier JOIN s_Supplier ON Supplier.S4SupplierCode  = s_Supplier.SupNo
       ORDER BY
         ExtSupNo ASC
+	DECLARE s_SupplierCursorByExtSupNo CURSOR FOR
+      SELECT
+        SupNo, SupCut, SupCom, SupMei1, SupMei2, SupMei3, SupMei4,
+        SupRes1, SupRes2, SupCry, SupCty, SupZip,
+        SupAdr1, SupAdr2, SupAdr3, SupAdr4, SupAdr5, SupAdr6, SupAdr7,
+        SupTel, SupFax, SupMail, SupTMei, SupTTel, SupTCom,
+		SupID1,SupMAD1,SupREM1,
+		SupID2,SupMAD2,SupREM2,
+		SupID3,SupMAD3,SupREM3,
+		SupID4,SupMAD4,SupREM4,
+		SupID5,SupMAD5,SupREM5,
+		SupID6,SupMAD6,SupREM6,
+		SupID7,SupMAD7,SupREM7,
+		SupID8,SupMAD8,SupREM8,
+		SupID9,SupMAD9,SupREM9,
+		SupID10,SupMAD10,SupREM10,
+		ExtSupNo
+      FROM
+		Supplier JOIN s_Supplier ON Supplier.SupplierCode = s_Supplier.ExtSupNo
+      ORDER BY
+        ExtSupNo ASC
+
     OPEN s_SupplierCursor;
     FETCH NEXT FROM s_SupplierCursor
     INTO
@@ -168,7 +193,20 @@ begin try
 		SupplierEmail10=@SupMAD10,							
 		SupplierContactperson10=@SupREM10		
       WHERE 
-        SupplierCode = @ExtSupNo;
+        S4SupplierCode = @SupNo;
+		
+		Select @SupplierCode=SupplierCode From Supplier Where Supplier.S4SupplierCode = @SupNo;
+		IF @SupplierCode != null and @SupplierCode != @ExtSupNo
+			begin
+				Update Supplier
+				Set S4SupplierCode = null
+				Where SupplierCode = @SupplierCode
+
+				Update Supplier
+				Set S4SupplierCode = @SupNo
+				Where SupplierCode = @ExtSupNo
+			end
+		
     FETCH NEXT FROM s_SupplierCursor
     INTO
       @SupNo, @SupCut, @SupCom, @SupMei1, @SupMei2, @SupMei3, @SupMei4,
@@ -188,6 +226,99 @@ begin try
     END -- roop end
     CLOSE s_SupplierCursor;
     DEALLOCATE s_SupplierCursor;
+
+	OPEN s_SupplierCursorByExtSupNo;
+    FETCH NEXT FROM s_SupplierCursorByExtSupNo
+    INTO
+      @SupNo, @SupCut, @SupCom, @SupMei1, @SupMei2, @SupMei3, @SupMei4,
+      @SupRes1, @SupRes2, @SupCry, @SupCty, @SupZip,
+      @SupAdr1, @SupAdr2, @SupAdr3, @SupAdr4, @SupAdr5, @SupAdr6, @SupAdr7,
+      @SupTel, @SupFax, @SupMail, @SupTMei, @SupTTel, @SupTCom,
+	  @SupID1,@SupMAD1,@SupREM1,
+	  @SupID2,@SupMAD2,@SupREM2,
+	  @SupID3,@SupMAD3,@SupREM3,
+	  @SupID4,@SupMAD4,@SupREM4,
+	  @SupID5,@SupMAD5,@SupREM5,
+	  @SupID6,@SupMAD6,@SupREM6,
+	  @SupID7,@SupMAD7,@SupREM7,
+	  @SupID8,@SupMAD8,@SupREM8,
+	  @SupID9,@SupMAD9,@SupREM9,
+	  @SupID10,@SupMAD10,@SupREM10,@ExtSupNo;
+    WHILE @@FETCH_STATUS = 0
+    BEGIN    
+      UPDATE Supplier
+      SET
+        Name1 = @SupMei1,
+        Name2 = @SupMei2,
+        Name3 = @SupMei3,
+        Name4 = @SupMei4, 
+        SearchTerm1 = @SupRes1,
+        SearchTerm2 = @SupRes2, 
+        Address1 = CASE WHEN @SupCry = 'JP' THEN @SupAdr1 ELSE @SupAdr5 END, 
+        Address2 = CASE WHEN @SupCry = 'JP' THEN @SupAdr2 ELSE @SupAdr6 END, 
+        Address3 = CASE WHEN @SupCry = 'JP' THEN @SupAdr7 ELSE @SupAdr2 END, 
+        PostalCode  = @SupZip,
+        CountryCode = @SupCry,
+        RegionCode  = @SupCty,
+        Telephone   = @SupTel, 
+        Fax         = @SupFax,
+        Comment     = @SupCom,
+        UpdatedBy   = 0,
+        UpdateDate  = GETDATE(),
+		isDisabled	= CASE WHEN @SupCut is null THEN 0 ELSE 1 END,
+		Email=@SupMail,
+		S4SupplierCode=@SupNo,						
+		SupplierEmailID1=@SupID1,							
+		SupplierEmail1=@SupMAD1,							
+		SupplierContactperson1=@SupREM1,							
+		SupplierEmailID2=@SupID2,							
+		SupplierEmail2=@SupMAD2,						
+		SupplierContactperson2=@SupREM2,							
+		SupplierEmailID3=@SupID3,							
+		SupplierEmail3=@SupMAD3,							
+		SupplierContactperson3=@SupREM3,							
+		SupplierEmailID4=@SupID4,							
+		SupplierEmail4=@SupMAD4,							
+		SupplierContactperson4=@SupREM4,							
+		SupplierEmailID5=@SupID5,							
+		SupplierEmail5=@SupMAD5,							
+		SupplierContactperson5=@SupREM5,							
+		SupplierEmailID6=@SupID6,							
+		SupplierEmail6=@SupMAD6,							
+		SupplierContactperson6=@SupREM6,							
+		SupplierEmailID7=@SupID7,							
+		SupplierEmail7=@SupMAD7,							
+		SupplierContactperson7=@SupREM7,							
+		SupplierEmailID8=@SupID8,							
+		SupplierEmail8=@SupMAD8,							
+		SupplierContactperson8=@SupREM8,							
+		SupplierEmailID9=@SupID9,							
+		SupplierEmail9=@SupMAD9,							
+		SupplierContactperson9=@SupREM9,							
+		SupplierEmailID10=@SupID10,							
+		SupplierEmail10=@SupMAD10,							
+		SupplierContactperson10=@SupREM10		
+      WHERE 
+        SupplierCode = @ExtSupNo;
+    FETCH NEXT FROM s_SupplierCursorByExtSupNo
+    INTO
+      @SupNo, @SupCut, @SupCom, @SupMei1, @SupMei2, @SupMei3, @SupMei4,
+      @SupRes1, @SupRes2, @SupCry, @SupCty, @SupZip,
+      @SupAdr1, @SupAdr2, @SupAdr3, @SupAdr4, @SupAdr5, @SupAdr6, @SupAdr7,
+      @SupTel, @SupFax, @SupMail, @SupTMei, @SupTTel, @SupTCom,
+	  @SupID1,@SupMAD1,@SupREM1,
+	  @SupID2,@SupMAD2,@SupREM2,
+	  @SupID3,@SupMAD3,@SupREM3,
+	  @SupID4,@SupMAD4,@SupREM4,
+	  @SupID5,@SupMAD5,@SupREM5,
+	  @SupID6,@SupMAD6,@SupREM6,
+	  @SupID7,@SupMAD7,@SupREM7,
+	  @SupID8,@SupMAD8,@SupREM8,
+	  @SupID9,@SupMAD9,@SupREM9,
+	  @SupID10,@SupMAD10,@SupREM10,@ExtSupNo;
+    END -- roop end
+    CLOSE s_SupplierCursorByExtSupNo;
+    DEALLOCATE s_SupplierCursorByExtSupNo;
  commit transaction;
 end try
 begin catch
@@ -198,5 +329,4 @@ begin catch
  rollback transaction;
 end catch
 GO
-
 
